cb988aa48fa8f00d73b0ee67f2a5e9d4023ee7f1
Ma7moud-ibrahim <ahmed.01090223081@example.com>
2026-09-10 04:38:49 +0300

    create fetch method

--- lib/cubits/notes/notes_cubit.dart
+++ lib/cubits/notes/notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  Future<void> fatchAllNotes() async {
+    try {
+      var noteBox = Hive.box<NoteModel>(kNoteBox);
+      List<NoteModel> notes = noteBox.values.toList();
+      emit(NotesSucess(notes: notes));
+    } catch (e) {
+      emit(NotesFailure(errorMessage: e.toString()));
+    }
+  }
 }
