13bd1087784ba50236f89f7d63c119020194ebd3
Ma7moud-ibrahim <ahmed.01090223081@example.com>
2026-09-12 04:36:42 +0300

    delete note

--- lib/widgets/note_items.dart
+++ lib/widgets/note_items.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes/notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
@@ -48,7 +50,10 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                  BlocProvider.of<NotesCubit>(context).fatchAllNotes();
+                },
                 icon: Icon(Icons.delete, color: Colors.black, size: 35),
               ),
             ),
